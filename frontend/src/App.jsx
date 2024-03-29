886ad18b3cf9e4fded80dd7a0d79fc41d5ab8dcc
Abhinandan <jhaabhinandan55@gmail.com>
2024-03-30 04:31:18 +0530

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='bg-red-500 text-white'>
-      Abhinandan
+      <h1>My name is Abhinandan Jha</h1>
     </div>
   )
 }
