8633e4cc1069e9b9bb2e07a6990101f69f8322ce
Abhinandan <jhaabhinandan55@gmail.com>
2024-04-10 03:26:48 +0530

    CRUD operations completed

--- frontend/src/pages/DeleteBook.jsx
+++ frontend/src/pages/DeleteBook.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+import BackButton from './../components/BackButton';
+import Spinner from './../components/Spinner';
+import axios from 'axios';
 
 const DeleteBook = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  
+  const handleDeleteBook = () => { 
+    setLoading(true);
+    axios.delete(`http://localhost:5555/books/${id}`)
+    .then(()=>{
+      setLoading(false);
+      navigate('/');
+    }).catch((error)=>{
+      setLoading(false);
+      alert("Some error occurred. Please try again")
+      console.log(error);
+    })
+  }
+
   return (
-    <div>
-      Delete Books
+    <div className='p-4'>
+      <BackButton/>
+      <h1 className='text-3xl my-4'>Delete Book</h1>
+      {loading ? <Spinner/> : ''}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className='text-2xl'>Are you sure you want to delete this book?</h3>
+
+        <button className='p-4 bg-red-600 text-white m-8 w-full' onClick={handleDeleteBook}>Yes, delete it!
+        </button>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -1,34 +1,35 @@
-import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import  axios  from "axios";
-import BackButton from "./../components/BackButton";
-import Spinner from "./../components/Spinner";
+import React, { useState, useEffect } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 const EditBook = () => {
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [pages, setPages] = useState("");
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [pages, setPages] = useState('');
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const { id } = useParams();
+  const {id} = useParams();
+  const { enqueueSnackbar } = useSnackbar();
 
   useEffect(() => {
     setLoading(true);
     axios.get(`http://localhost:5555/books/${id}`)
     .then((response) => {
-      setTitle(response.data.title);
-      setAuthor(response.data.author);
-      setPages(response.data.pages);
-      setLoading(false);
-    })
-    .catch((error) => {
-      setLoading(false);
-      alert('Some error occured. Please try again')
-      console.log(error.message);
-    })
-  },[]);
-
-  const handleSavebook = () => {
+        setAuthor(response.data.author);
+        setPages(response.data.pages, )
+        setTitle(response.data.title)
+        setLoading(false);
+      }).catch((error) => {
+        setLoading(false);
+        alert('An error happened. Please Chack console');
+        console.log(error);
+      });
+  }, [])
+  
+  const handleEditBook = () => {
     const data = {
       title,
       author,
@@ -36,64 +37,59 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .post(`http://localhost:5555/books`, data)
+      .put(`http://localhost:5555/books/${id}`, data)
       .then(() => {
         setLoading(false);
-        navigate("/");
+        enqueueSnackbar('Book Edited successfully', { variant: 'success' });
+        navigate('/');
       })
       .catch((error) => {
         setLoading(false);
-        alert("An error occured. Please check console!");
+        // alert('An error happened. Please Chack console');
+        enqueueSnackbar('Error', { variant: 'error' });
         console.log(error);
       });
   };
 
   return (
-    <div className="p-4">
+    <div className='p-4'>
       <BackButton />
-      <h1 className="text-3xl my-4">Edit book</h1>
-      {loading ? <Spinner /> : ""}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
-        <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Title</label>
+      <h1 className='text-3xl my-4'>Edit Book</h1>
+      {loading ? <Spinner /> : ''}
+      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Title</label>
           <input
-            type="text"
+            type='text'
             value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            onChange={(e) => setTitle(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2 w-full'
           />
         </div>
-
-        <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Author</label>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Author</label>
           <input
-            type="text"
+            type='text'
             value={author}
-            onChange={(e) => {
-              setAuthor(e.target.value);
-            }}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            onChange={(e) => setAuthor(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
           />
         </div>
-
-        <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Pages</label>
+        <div className='my-4'>
+          <label className='text-xl mr-4 text-gray-500'>Pages</label>
           <input
-            type="text"
+            type='number'
             value={pages}
-            onChange={(e) => {
-              setPages(e.target.value);
-            }}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
+            onChange={(e) => setPages(e.target.value)}
+            className='border-2 border-gray-500 px-4 py-2  w-full '
           />
         </div>
-
-        <button className="p-2 bg-sky-300 m-8" onClick={handleSavebook}>Save</button>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>
+          Edit
+        </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default EditBook;
+export default EditBook
