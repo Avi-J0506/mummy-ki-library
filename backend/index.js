3854c1dee36fdafd95f2106d20cae5dd49a87223
Abhinandan Jha <92037313+Avi-J0506@users.noreply.github.com>
2024-07-17 18:26:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 import express, { response } from "express";
 import { PORT, mongoDB_URL } from "./config.js";
 import mongoose from "mongoose";
-import booksRoute from "./routes/booksRoute.js";
-import { Book } from "./models/bookModel.js";
+import trucksRoute from "./routes/trucksRoute.js";
+import { Truck } from "./models/truckModel.js";
 import cors from "cors";
 
 const app = express();
@@ -22,11 +22,11 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   console.log(req);
-  return res.status(200).send("Mummy ki library");
+  return res.status(200).send("Trucks details");
 });
 
 //Using booksRoute
-app.use("/books", booksRoute);
+app.use("/trucks", trucksRoute);
 
 //MongoDB connection
 mongoose
